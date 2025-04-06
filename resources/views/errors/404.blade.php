5b4919327528f9f1abb66b0ec985f53c7a0d0a0a
Raaki30 <rafifradina@gmail.com>
2025-04-06 18:48:22 +0700

    create error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
-@extends('errors::minimal')
+@extends('errors.minimal')
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@section('title', 'Waduh! Kayaknya kamu nyasar deh')
+@section('code', 'Ngapain disini? Mending balik')
+@section('message', 'Error code : 404')
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Server Error'))
+@section('title', __('Oops! Kayaknya sedang ada masalah...'))
+@section('code', 'Oops! Kayaknya sedang ada masalah...')
+@section('message', __('Error code : 500'))
